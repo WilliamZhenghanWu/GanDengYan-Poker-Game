105527cd61241e66fe0747cec6b19c71b7fbac3e
WilliamWu <101683154+WilliamZhenghanWu@users.noreply.github.com>
2024-02-25 16:59:04 +0800

    Add files via upload

--- 干瞪眼.cpp
+++ 干瞪眼.cpp
@@ -1,5 +1,10 @@
 #include<bits/stdc++.h>
+#include<windows.h>
+#include<sstream>
 using namespace std;
+
+/*       定义变量         */ 
+
 string cards[55];//牌组 
 int washCard_1;// 洗牌变量1 
 int washCard_2;// 洗牌变量2
@@ -7,9 +12,75 @@ string TempCard;//临时牌变量，用于洗牌做替换时临时存储
 string MyCard[6];//玩家的牌 
 string AI1Card[6];//AI1的牌 
 string AI2Card[6];//AI2的牌 
-string Master="Me";//庄家 
+string Master="Me";//庄家
+int choice=-1;//开始界面选项 
+int choice_in_game_num;//转换为数字的游戏中出牌选项 
+string Cards_Be_Chosen[6];//被选中的牌
+int Cards_Be_Chosen_Num;//在循环中用于计数 
+
+/* 理牌函数 */
+
+void range_cards(){
+    auto cmp = [](const string &a, const string &b) {
+        map<string, int> rank;
+        rank["3"] = 1; rank["4"] = 2; rank["5"] = 3; rank["6"] = 4; 
+        rank["7"] = 5; rank["8"] = 6; rank["9"] = 7; rank["10"] = 8; 
+        rank["J"] = 9; rank["Q"] = 10; rank["K"] = 11; rank["A"] = 12; 
+        rank["2"] = 13; rank["小王"] = 0; rank["大王"] = 0; rank["广告"] = 0;
+        return rank[a] < rank[b];
+    };// 对牌进行排序
+    sort(MyCard, MyCard+6, cmp);
+    sort(AI1Card, AI1Card+6, cmp);
+    sort(AI2Card, AI2Card+6, cmp);
+};
+
+/* 单张牌是否符合逻辑函数 */
+
+bool SingleCardIsValid(const string &OthersCard, const string &OnesCard){
+        map<string, int> rank;
+        rank["3"] = 1; rank["4"] = 2; rank["5"] = 3; rank["6"] = 4; 
+        rank["7"] = 5; rank["8"] = 6; rank["9"] = 7; rank["10"] = 8; 
+        rank["J"] = 9; rank["Q"] = 10; rank["K"] = 11; rank["A"] = 12; 
+        rank["2"] = 13; rank["小王"] = -1; rank["大王"] = -1; rank["广告"] = -1;
+        if(rank[OnesCard]-rank[OthersCard]==1)	return 1;
+        else return 0;
+};
+
+/* 更新手牌 */
+void Player_Cards_Update(string MyCard[]){
+    // 从玩家手牌中删除用户选中的牌
+    string NewHand[6];
+    int NewHandSize = 0;
+    bool removed[6] = {false};
+    for (int i = 0; i < 6; i++) {
+        bool chosen = false;
+        for (int j = 0; j < Cards_Be_Chosen_Num; j++) {//判断牌是否被选中，并标记 
+            if (MyCard[i] == Cards_Be_Chosen[j] && !removed[j]) {
+                chosen = true;
+                removed[j] = true;
+                break;
+            }
+        }
+        if (!chosen) {//删除 
+            NewHand[NewHandSize] = MyCard[i];
+            NewHandSize++;
+        }
+        else{
+        	NewHand[NewHandSize] = "";
+        	NewHandSize++;
+		}
+	}
+
+    // 更新玩家手牌
+    for (int i = 0; i < NewHandSize; i++) {
+        MyCard[i] = NewHand[i];
+    }
+};
+
 int main(){
+	
 	/*      创建牌组       */
+	
 	cards[0]="3";
 	cards[1]="3";
 	cards[2]="3";
@@ -65,7 +136,9 @@ int main(){
     cards[52]="小王";
 	cards[53]="大王";
 	cards[54]="广告";
+	
 	/*        洗牌         */ 
+	
 	srand(time(0));//将时间设为随机种子 
 	for(int i=1;i<=rand();i++){
 	    int washCard_1=rand()%55;//将其取0-54的随机数 
@@ -85,6 +158,7 @@ int main(){
     return 0;*/
     
     /*     发牌      */
+    
     if(Master=="Me"){
 	    MyCard[0]=cards[0];
 	    AI1Card[0]=cards[1];
@@ -155,17 +229,7 @@ int main(){
 	cout<<"第"<<i<<"张："<<AI2Card[i]<<endl;*/
 	
 	/*      排序           */
-    auto cmp = [](const string &a, const string &b) {
-        map<string, int> rank;
-        rank["3"] = 1; rank["4"] = 2; rank["5"] = 3; rank["6"] = 4; 
-        rank["7"] = 5; rank["8"] = 6; rank["9"] = 7; rank["10"] = 8; 
-        rank["J"] = 9; rank["Q"] = 10; rank["K"] = 11; rank["A"] = 12; 
-        rank["2"] = 13; rank["小王"] = 14; rank["大王"] = 15; rank["广告"] = 16;
-        return rank[a] < rank[b];
-    };// 对牌进行排序
-    sort(MyCard, MyCard+6, cmp);
-    sort(AI1Card, AI1Card+6, cmp);
-    sort(AI2Card, AI2Card+6, cmp);
+	range_cards();
 
     /*测试排序后的牌是否正常 
     cout << "我的牌：" << endl;
@@ -183,6 +247,62 @@ int main(){
         cout << "第" << i+1 << "张：" << AI2Card[i] << endl;
     }*/
     
-    /*     出牌        */
-	 
+    /*     开始逻辑        */
+    
+    while(1){
+    system("color 03");//设置颜色 
+    system("cls");
+    cout<<"_______________________________________________________"<<endl; //开始界面 
+	cout<<"|                    干瞪眼游戏                       |"<<endl; 
+	cout<<"-------------------------------------------------------"<<endl; 	
+	cout<<"|                     更新日志                        |"<<endl; 
+	cout<<"| 1.现在有了开始界面                                  |"<<endl; 
+	cout<<"| 2.日志可以显示                                      |"<<endl; 
+	cout<<"-------------------------------------------------------"<<endl; 
+	cout<<"|                     游戏选项                        |"<<endl;	
+    cout<<"| 1.开始游戏                                          |"<<endl; 
+	cout<<"| 2.请等待下一个版本                                  |"<<endl; 
+	cout<<"-------------------------------------------------------"<<endl; 	
+	cout<<"| InfDev1.1                                           |"<<endl;
+	cout<<"-------------------------------------------------------"<<endl; 
+	cout<<">>>";
+	cin>>choice;
+	if(choice==1){//开始游戏 
+	    while(choice){
+	    if(Master=="Me"){
+        system("cls");
+        cout << "我的牌：" << endl;
+        for (int i = 0; i < 6; i++) {
+        cout << "第" << i+1 << "张：" << MyCard[i] << endl;
+ 		}
+ 		cout<<"你选择出第几张？>>>";
+		Cards_Be_Chosen_Num=0;
+		while(cin>>choice_in_game_num){
+            if(choice_in_game_num==-1)break;
+			Cards_Be_Chosen[Cards_Be_Chosen_Num]=MyCard[choice_in_game_num-1];
+		    Cards_Be_Chosen_Num++;	
+		}
+		
+		/*测试牌是否被正确选中 
+		for(int i=0;i<=5;i++)
+		cout<<"被选中的第"<<i+1<<"张牌："<<Cards_Be_Chosen[i]<<endl;
+		Sleep(5000);
+		*/
+		
+		if(Cards_Be_Chosen_Num==1){
+			Player_Cards_Update(MyCard);//更新牌 
+			range_cards();//理牌 
+		}
+	    }
+    }
+	} 
+	else if(choice==2){//等待下一个版本 
+	    for(int i=3;i>0;i--){
+	        system("cls");
+	        cout<<"请等待下一个版本！" <<endl;
+	        cout<<i<<"秒后回到主界面……"<<endl;
+	        Sleep(1000);
+	    }
+    }
+}
 }
